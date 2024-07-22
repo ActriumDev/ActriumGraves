b783da5335cc261d46fadb6e00765bc6fb5da531
Skitbet <skeeterld06@gmail.com>
2024-07-22 09:13:23 -0400

    base menu done

--- src/main/java/xyz/actrium/graves/menu/impl/GravesMenu.java
+++ src/main/java/xyz/actrium/graves/menu/impl/GravesMenu.java
@@ -2,26 +2,38 @@ package xyz.actrium.graves.menu.impl;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import xyz.actrium.graves.ActriumGraves;
 import xyz.actrium.graves.GraveManager;
 import xyz.actrium.graves.death.Death;
 import xyz.actrium.graves.item.ItemManager;
 import xyz.actrium.graves.menu.Menu;
+import xyz.actrium.graves.util.CC;
 import xyz.actrium.graves.util.StringUtils;
 import xyz.actrium.graves.util.TimeUtil;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.LogRecord;
 
 public class GravesMenu extends Menu {
+
+    private final static NamespacedKey KEY = new NamespacedKey(ActriumGraves.get(), "gravesMenu");
+
+    private HashMap<ItemStack, Death> deathsToItems;
+
     protected GravesMenu(Player player) {
         super(player);
+        deathsToItems = new HashMap<>();
     }
 
     @Override
@@ -39,13 +51,17 @@ public class GravesMenu extends Menu {
                 ArrayList<String> lore = new ArrayList<>();
                 int distance = (int)death.getGraveLocation().distance(player.getLocation());
                 lore.add(ChatColor.GRAY + "━━━━━━━━━━━━━━━━━━━━━");
+                // TODO: configuration
                 lore.add(ChatColor.WHITE + "Distance: " + distance + " blocks");
                 lore.add(ChatColor.WHITE + "Time Till Deletion: " + TimeUtil.convertLongToRemainingDuration(death.getTimeOfDeath())); // TODO: Get time till deletion
                 lore.add(ChatColor.GRAY + "━━━━━━━━━━━━━━━━━━━━━");
-
+                // TODO: configuration
+                lore.add(ChatColor.RED + "Right Click to delete.");
+                lore.add(ChatColor.RED + "Left Click to teleport.");
                 meta.setLore(lore);
 
                 stack.setItemMeta(meta);
+                deathsToItems.put(stack, death);
                 return stack;
             }).toList();
 
@@ -56,12 +72,34 @@ public class GravesMenu extends Menu {
 
     @Override
     public void handleClick(InventoryClickEvent event) {
-        if (event.getCurrentItem().getType() == Material.GRAY_STAINED_GLASS_PANE) return;
-
         ItemStack clickItem = event.getCurrentItem();
+        if (clickItem == null || clickItem.getType() != Material.ENDER_EYE) return;
 
-        // TODO: get grave from this item
+        Death death = deathsToItems.get(clickItem);
+        if (death == null) {
+            player.sendMessage(CC.translate("&cSomething has went wrong, please try again."));
+            return;
+        }
 
+        Player player = (Player) event.getWhoClicked();
+
+        // handle left click
+        if (event.isLeftClick()) {
+
+            // TODO: configuration
+            player.teleport(death.getGraveLocation());
+            player.sendMessage(ChatColor.YELLOW + "Teleporting to grave...");
+            player.closeInventory();
+
+        }
+        else if (event.isRightClick()) {
+
+            // TODO: configuration
+            player.sendMessage(ChatColor.RED + "Deleting this grave...");
+            GraveManager.get().removeGrave(death);
+            player.closeInventory();
+
+        }
 
     }
 }
